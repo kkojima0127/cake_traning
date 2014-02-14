f71e5927d804f7b91228001950726d31749f18aa
Kazuya Kojima <ypcjh662@yahoo.co.jp>
2014-02-14 22:23:56 +0900

    Initial Setup

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'lgjgh49795jdbvnazplfgj850n17dbvne907mzngg');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '10575629676795001057295676319486980');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
